456f92c33ece3aab34f847abf7ec158618a48513
Raufull Islam Rauf <152892595+Rauf-17@users.noreply.github.com>
2024-02-18 00:46:17 +0600

    Add files via upload
    First program of basics

---
+++ Data Type and Operators.py
@@ -0,0 +1,130 @@
+ 
+
+# In[1]: #Data Types
+
+a = 10
+print(a)
+
+
+# In[2]:
+
+a = "This is lab 1"
+print(a)
+
+
+# In[3]:
+
+
+print(type(a))
+
+
+# In[4]:
+
+
+a = 5.6
+print(type(a))
+
+
+# In[7]:
+
+
+flag = True;
+print(flag)
+
+
+# In[8]:
+
+
+print(type(flag))
+
+
+# In[14]:
+
+
+#Operators
+#Arithmatic Operator(+,-,/,%,//,**)
+
+a = 10
+b = 3
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+print(a**b)
+
+
+# In[22]:
+
+
+#Logical Operator(>,<,>=,<=, and, or)
+a = 2
+b = 3
+c = 10
+d = 2
+print(c>=d)
+print(c<d)
+print(c == d)
+print(c != d)
+print (a<=b or b>c)
+print(a<=b and b>c)
+
+
+
+# In[25]:
+
+
+# Conditionals (if else, elif, do while etc)
+
+number = int(input("Please Enter a number"))
+print(number)
+
+if(number % 2 == 0):
+    print("Number is Even")
+else: print("Number is Odd")
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+age = int(input("Please Enter age: "))
+print(age)
+
+if(age >= 18):
+    print("Adult")
+    print("Aged")
+else:
+    print("Underaged")
+
+
+
+# In[ ]:
+
+
+age = int(input("Please Enter Age"))
+print(age)
+if(age>=18):
+    print("Adult")
+    print("Aged")
+else:
+    print("Underaged")
+
+
+# In[ ]:
+
+
+
+
